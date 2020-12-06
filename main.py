e8557898d1e37f2228c71d48c77ac8145988af09
tugrulyildirim.com <58388980+developertugrul@users.noreply.github.com>
2020-12-06 13:52:50 +0300

    new

--- main.py
+++ main.py
@@ -1,47 +1,92 @@
-from art import logo
+############### Blackjack Project #####################
 
-print(logo)
+# Difficulty Normal 😎: Use all Hints below to complete the project.
+# Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
+# Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
+# Difficulty Expert 🤯: Only use Hint 1 to complete the project.
 
+############### Our Blackjack House Rules #####################
 
-# add function
-def add(first_number, second_number):
-    return first_number + second_number
+## The deck is unlimited in size.
+## There are no jokers.
+## The Jack/Queen/King all count as 10.
+## The the Ace can count as 11 or 1.
+## Use the following list as the deck of cards:
+## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+## The cards in the list have equal probability of being drawn.
+## Cards are not removed from the deck as they are drawn.
+## The computer is the dealer.
 
+##################### Hints #####################
 
-# Subtract function
-def subtract(first_number, second_number):
-    return first_number - second_number
+# Hint 1: Go to this website and try out the Blackjack game:
+#   https://games.washingtonpost.com/games/blackjack/
+# Then try out the completed Blackjack project here:
+#   http://blackjack-final.appbrewery.repl.run
 
+# Hint 2: Read this breakdown of program requirements:
+#   http://listmoz.com/view/6h34DJpvJBFVRlZfJvxF
+# Then try to create your own flowchart for the program.
 
-# Divide function
-def divide(first_number, second_number):
-    return first_number / second_number
+# Hint 3: Download and read this flow chart I've created:
+#   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
 
+# Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
+# 11 is the Ace.
+# cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-# Multiply function
-def multiply(first_number, second_number):
-    return first_number * second_number
+# Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
+# user_cards = []
+# computer_cards = []
 
+# Hint 6: Create a function called calculate_score() that takes a List of cards as input
+# and returns the score.
+# Look up the sum() function to help you do this.
 
-# fonksiyonları listeledğimiz bir dictionary
-operations = {
-    "+": add,
-    "-": subtract,
-    "*": multiply,
-    "/": divide
-}
+# Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
 
-is_finished = False
+# Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
 
-while not is_finished:
-    first_number = int(input("Give us the first number : "))
-    second_number = int(input("Give us the second number : "))
-    for symbol in operations:
-        print(symbol)
-    operation = input("Select the operation above : ")
-    function = operations[operation]
-    answer = function(first_number, second_number)
-    print(f"{first_number} {operation} {second_number} = {answer}")
-    wish = input("Do you want to make another calculation : (Y) or (N) ").lower()
-    if wish == "n":
-        is_finished = True
+# Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
+
+# Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
+
+# Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
+
+# Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
+
+# Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
+
+# Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
+import random
+from art import blackjack
+
+print(blackjack)
+total_cash = 2000
+bets = [10, 20, 30, 50, 100, 200, 500, 1000]
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+player_cards = []
+dealer_cards = []
+
+for bet in bets:
+    print(bet)
+
+first_bet = int(input("Please, select the bet above : "))
+
+print(f"Your bet is {first_bet}")
+
+player_cards.append(random.choice(cards))
+player_cards.append(random.choice(cards))
+dealer_cards.append(random.choice(cards))
+dealer_cards.append(random.choice(cards))
+
+print(f" Your cards : {player_cards[0]} - {player_cards[1]}")
+print(f" Dealer's cards : {dealer_cards[0]} - ***")
+
+is_contunie = input("Do you want extra card. Type (Y) for yes or (N) for no")
+
+if is_contunie == "Y":
+    player_cards.append(random.choice(cards))
+
+is_contunie = input("Do you want extra card. Type (Y) for yes or (N) for no")
